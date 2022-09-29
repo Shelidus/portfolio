2c98315d5ee3365613d3b477c153839bb5005c01
Shelidus <114400197+Shelidus@users.noreply.github.com>
2022-09-29 08:46:06 +0530

    Create New.js

---
+++ New/New.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        bugfixes: true,
+        modules: false
+      }
+    ]
+  ]
+};
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
